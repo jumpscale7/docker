185ce4779f2d674e62ab4c95f3a4e990778fdef8
despiegk <kristof@incubaid.com>
2014-12-20 10:27:29 +0200

    init

--- image_js/jumpscale.sh
+++ image_js/jumpscale.sh
@@ -4,8 +4,6 @@ source /build/buildconfig
 set -x
 
 
-$minimal_apt_get_install mc python-git git ssh python2.7 python-requests python-apt openssl ca-certificates ipython wget -y
-cd /tmp;wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-cd /tmp;python get-pip.py
-apt-get install libmhash2 libpython-all-dev python-redis python-hiredis -y
+curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/install/install_python_web.sh | bash
+
 
