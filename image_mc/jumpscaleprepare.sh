ac9527cd163b48bf801f3fb7ff5126e6e7c82062
kristof de spiegeleer <kristof@incubaid.com>
2015-01-01 05:52:06 +0200

    Update jumpscaleprepare.sh

--- image_mc/jumpscaleprepare.sh
+++ image_mc/jumpscaleprepare.sh
@@ -4,9 +4,10 @@ source /build/buildconfig
 set -x
 
 
-$minimal_apt_get_install mc python-git git ssh python2.7 python-requests python-apt openssl ca-certificates ipython wget -y
+$minimal_apt_get_install mc python-git git ssh python2.7 python-requests python-apt openssl ca-certificates wget rsync -y
 cd /tmp;wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 cd /tmp;python get-pip.py
+
 apt-get install libmhash2 libpython-all-dev python-redis python-hiredis -y
 
 #change passwd
